852bd67c26b9fd025da65b48578cb51fa2f01079
Ben Tupper <btupper@bigelow.org>
2021-11-24 08:57:07 -0500

    fix example to truncate anonymized fields within range of provided example, rename from action_inputto actine_dataset

--- R/io.R
+++ R/io.R
@@ -6,7 +6,7 @@
 #' @param crs character or numeric, if \code{form} is 'sf' then pass this to
 #'   \code{\link[sf]{st_as_sf}}
 #' @return tibble or simple feature
-read_input <- function(filename = system.file("exdata/example_input.csv.gz",
+read_dataset <- function(filename = system.file("exdata/example_input.csv.gz",
                                               package = "calanusthreshold"),
                        form = c("tibble", "sf")[2],
                        crs = 4326){
@@ -15,23 +15,26 @@ read_input <- function(filename = system.file("exdata/example_input.csv.gz",
   }
   x <- suppressMessages(readr::read_csv(filename))
   if (tolower(form[1]) == 'sf')  x <- sf::st_as_sf(x, wkt = "geometry", crs = crs)
+  x
 }
 
 
-#' Given an input dataset, create a subsample and possibly anonymize ID and location
+#' Given a dataset, create a subsample and possibly anonymize ID and location
 #'
 #' @param x sf or tibble input dataset
 #' @param n the number of subsamples, or NA to use all
 #' @param anonymize logical, if TRUE anonymize the data
 #' @param noise numeric, fraction of noise to add to each numeric variable (column)
 #' @return sf or tibble subset, possibly anonymized
-create_subsample <- function(x,
+subsample_dataset <- function(x,
                              n = c(NA, 500)[2],
                              anonymize = TRUE,
                              noise = 0.2){
   if (!inherits(x, 'sf')){
     isTibble <- TRUE
     x <- sf::st_as_sf(x, wkt = "geometry", crs = 4326)
+  } else {
+    isTibble <- FALSE
   }
 
   if (!is.na(n[1])) x <- dplyr::slice_sample(x, n = n[1])
@@ -43,12 +46,25 @@ create_subsample <- function(x,
 
     klass <- sapply(x, function(x) paste(class(x), collapse = ","))
     N <- nrow(x)
+
+    # given a vector and min/max values truncate the vector such that mn <= x <= mx
+    trunc_range <- function(x, mn, mx){
+      x[x < mn] <- mn
+      x[x > mx] <- mx
+      x
+    }
     for (nm in names(klass)){
       if (klass[[nm]] == "numeric"){
         r <- range(x[[nm]], na.rm = TRUE)
         amount <- noise * (r[2] - r[1])
-        x[[nm]] <- jitter(x[[nm]], amount = amount)
-
+        # we allow lon/lat to range more freely
+        if (nm == "latitude"){
+          r <- c(-90, 90)
+        }
+        if (nm == "longitude"){
+          r <- c(-180, 180)
+        }
+        x[[nm]] <- trunc_range(jitter(x[[nm]], amount = amount), r[1], r[2])
       }
     }
     xy <- as.matrix(x |> sf::st_drop_geometry() |> dplyr::select("longitude", "latitude"))
@@ -65,4 +81,29 @@ create_subsample <- function(x,
       dplyr::mutate(geometry = g)
   }
   x
+}
+
+#' Write a dataset
+#'
+#' @param x sf or tibble input-style dataset
+#' @param filename character, the file to write to
+#' @return the provided sf or tibble
+write_dataset <- function(x, filename = "dataset.csv.gz"){
+  if (!inherits(x, 'sf')){
+    isTibble <- TRUE
+    x <- sf::st_as_sf(x, wkt = "geometry", crs = 4326)
+  } else {
+    isTibble <- FALSE
+  }
+  crs <- sf::st_crs(x)
+  g <- sf::st_as_text(sf::st_geometry(x))
+  y <- x |>
+     sf::st_drop_geometry() |>
+     dplyr::mutate(geometry = g) |>
+     readr::write_csv(filename)
+  if (!isTibble){
+    return(invisible(x))
+  } else {
+    return(invisible(y))
+  }
 }
--- README.md
+++ README.md
@@ -27,28 +27,28 @@ We provide an anonymized example dataset.
 
 ``` r
 library(calanusthreshold)
-x <- read_input()
+x <- read_dataset()
 x
 ```
 
     ## Simple feature collection with 500 features and 31 fields
     ## Geometry type: POINT
     ## Dimension:     XY
-    ## Bounding box:  xmin: -70.7319 ymin: 40.57081 xmax: -56.14393 ymax: 51.63103
+    ## Bounding box:  xmin: -70.85388 ymin: 40.54264 xmax: -55.5305 ymax: 51.67751
     ## Geodetic CRS:  WGS 84
     ## # A tibble: 500 × 32
-    ##    station  year  month longitude latitude `Calanus finmarchi… `Calanus finmarc…
-    ##  * <chr>   <dbl>  <dbl>     <dbl>    <dbl>               <dbl>             <dbl>
-    ##  1 A-01    2004.  6.98      -67.0     45.3               9398.            -6205.
-    ##  2 B-01    2009.  6.49      -63.2     49.1              40012.            18310.
-    ##  3 C-01    2006. 12.7       -65.4     50.2              -1817.            17998.
-    ##  4 C-01    2005.  3.12      -66.2     50.6               9261.            13804.
-    ##  5 A-01    2005.  0.802     -67.7     46.3               4051.            -8919.
-    ##  6 D-01    1997.  5.63      -64.6     44.3              18878.             2877.
-    ##  7 E-01    2011.  6.32      -70.6     48.1               5755.             7948.
-    ##  8 F-01    2018.  3.59      -59.4     47.3              10069.            -7545.
-    ##  9 E-01    2013.  5.98      -70.6     49.7                916.            -5300.
-    ## 10 G-01    2001.  9.49      -59.8     47.6              -2535.             7528.
+    ##    station  year month longitude latitude `Calanus finmarchi… `Calanus finmarch…
+    ##  * <chr>   <dbl> <dbl>     <dbl>    <dbl>               <dbl>              <dbl>
+    ##  1 A-01    2019.  4.01     -60.2     45.2                  0              33823.
+    ##  2 B-01    2014.  6.44     -67.6     47.8              13684.              6461.
+    ##  3 C-01    2018.  9.85     -68.1     41.2               6675.              5116.
+    ##  4 D-01    2020  10.2      -70.1     50.0                  0                  0 
+    ##  5 E-01    2020  10.6      -59.2     45.8              31330.                 0 
+    ##  6 F-01    2008.  2.51     -64.7     47.0               6878.              7678.
+    ##  7 A-01    2010.  1.89     -62.4     44.9              19216.              9855.
+    ##  8 A-01    2014.  4.89     -64.0     44.0               2474.                 0 
+    ##  9 G-01    2006.  4.37     -58.7     50.5               4054.             12186.
+    ## 10 H-01    2020   5.59     -68.1     44.7              12990.              3908.
     ## # … with 490 more rows, and 25 more variables: Calanus finmarchicus VI <dbl>,
     ## #   Calanus hyperboreus IV <dbl>, Calanus hyperboreus V <dbl>,
     ## #   Calanus hyperboreus VI <dbl>, Calanus glacialis IV <dbl>,
@@ -59,4 +59,4 @@ x
 
 But if you have your own dataset, just provide the filename and path.
 
-    x <- read_input("/path/to/input/data.csv")
+    x <- read_dataset("/path/to/input/data.csv")
